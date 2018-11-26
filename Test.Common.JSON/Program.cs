702f7b55f87dfe972281861c32248d1e7eca2908
Scott Bakker <scott@bakkergames.com>
2018-11-26 15:12:43 -0500

    test hg files

--- Test.Common.JSON/Program.cs
+++ Test.Common.JSON/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Common.JSON;
 using System;
+using System.IO;
 
 namespace Test.Common.JSON
 {
@@ -7,10 +8,21 @@ namespace Test.Common.JSON
     {
         static void Main(string[] args)
         {
-            JObject testObj = new JObject();
-            testObj.Add("Hello", -123.45e23);
-            Console.WriteLine("{\n  \"Hello\": -123.45e23\n}");
-            Console.WriteLine(testObj.ToString(JsonFormat.Indent));
+            //JObject testObj = new JObject();
+            //testObj.Add("Hello", -123.45e23);
+            //Console.WriteLine("{\n  \"Hello\": -123.45e23\n}");
+            //Console.WriteLine(testObj.ToString(JsonFormat.Indent));
+            //Console.ReadLine();
+
+            string testFilename = "D:\\Projects\\Common.JSON\\TestData\\save3.hg";
+            string outTestFilename = "D:\\Projects\\Common.JSON\\TestData\\save3out_indent.json";
+            using (StreamReader sr = new StreamReader(testFilename))
+            {
+                JObject jTest = JObject.Parse(sr);
+                File.WriteAllText(outTestFilename, jTest.ToString(JsonFormat.Indent));
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press enter...");
             Console.ReadLine();
         }
     }
